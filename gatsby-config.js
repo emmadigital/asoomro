d5fd450fa2c717f1340de7a41bb79273093bee75
emmadigital <emma.digital.official@gmail.com>
2025-02-07 18:12:47 +0300

    fix favicon ext

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "../content/images/favicon.PNG", // Path is relative to the root
+          favicon: "../content/images/favicon.png", // Path is relative to the root
           siteName: "Amresha Soomro Profile", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
