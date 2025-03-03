a0f7b0082313bfceee2631440eafd5732421b4a1
emmadigital <emma.digital.official@gmail.com>
2025-03-03 17:38:11 +0300

    update sales file

--- src/pages/sales.js
+++ src/pages/sales.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { LegalSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
+import { ProjectsSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
 
 export default function SalesPage() {
   return (
     <>
       <Seo title="Sales" useTitleTemplate={true} noIndex={true} />
       <Page>
-        <LegalSection sectionId="sales" heading="Sales" />
+        <ProjectsSection sectionId="sales" heading="Sales" />
       </Page>
     </>
   );
